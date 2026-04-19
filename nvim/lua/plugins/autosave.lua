eb98ff98e6f88d4557aee2962c9d28d2c27a458a
Raymond Maung <ronokaoswe@gmail.com>
2026-04-19 16:52:13 -0500

    Remove deprecated execution_message from auto-save config

--- nvim/lua/plugins/autosave.lua
+++ nvim/lua/plugins/autosave.lua
@@ -2,9 +2,6 @@ return {
   "okuuva/auto-save.nvim",
   event = { "InsertLeave", "TextChanged" },
   opts = {
-    execution_message = {
-      enabled = false,
-    },
     debounce_delay = 1000,
     condition = function(buf)
       local filetype = vim.bo[buf].filetype
