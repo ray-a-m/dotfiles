8236464fcb706948b606387b96233ef82fa73d73
Raymond Maung <ronokaoswe@gmail.com>
2026-04-19 03:52:23 -0500

    Enable tinted variant for modus-themes

--- nvim/lua/plugins/colorschemes-light.lua
+++ nvim/lua/plugins/colorschemes-light.lua
@@ -1,5 +1,15 @@
 return {
   { "NLKNguyen/papercolor-theme", lazy = false, priority = 100 },
   { "reedes/vim-colors-pencil", lazy = false, priority = 100 },
-  { "miikanissi/modus-themes.nvim", lazy = false, priority = 100 },
+  {
+    "miikanissi/modus-themes.nvim",
+    lazy = false,
+    priority = 100,
+    opts = {
+      variants = {
+        modus_operandi = "tinted",
+        modus_vivendi = "tinted",
+      },
+    },
+  },
 }
