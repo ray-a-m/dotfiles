7baaa9ca7ff957b435565ec062efbc244ef776b4
Raymond Maung <ronokaoswe@gmail.com>
2026-04-19 04:00:03 -0500

    Add zen-mode and no-neck-pain writing plugins

---
+++ nvim/lua/plugins/writing.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    keys = {
+      { "<leader>z", "<cmd>ZenMode<cr>", desc = "Toggle Zen Mode" },
+    },
+    opts = {
+      window = {
+        width = 90,
+        options = {
+          number = false,
+          relativenumber = false,
+        },
+      },
+    },
+  },
+  {
+    "shortcuts/no-neck-pain.nvim",
+    cmd = "NoNeckPain",
+    keys = {
+      { "<leader>np", "<cmd>NoNeckPain<cr>", desc = "Toggle No Neck Pain (center buffer)" },
+    },
+    opts = {
+      width = 100,
+    },
+  },
+}
