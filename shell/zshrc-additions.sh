72c206872d9de01c78f1a7cd7e95413d98157eb8
Raymond Maung <ronokaoswe@gmail.com>
2026-04-18 21:28:50 -0500

    Add save function: one-shot add+commit+push

--- shell/zshrc-additions.sh
+++ shell/zshrc-additions.sh
@@ -5,5 +5,10 @@ alias pub="cd ~/scholarship/research-public"
 alias dots="cd ~/code/dotfiles"
 alias v="nvim"
 
+# One-shot: stage all, commit with "." message, and push
+save() {
+  git add -A && git commit -m "." && git push
+}
+
 # zoxide - smarter cd
 eval "$(zoxide init zsh)"
