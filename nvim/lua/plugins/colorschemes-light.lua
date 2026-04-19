eefd555745c733c090c9eb350a7561331cd59948
Raymond Maung <ronokaoswe@gmail.com>
2026-04-19 03:44:39 -0500

    Add modus-themes colorscheme

--- nvim/lua/plugins/colorschemes-light.lua
+++ nvim/lua/plugins/colorschemes-light.lua
@@ -1,4 +1,5 @@
 return {
   { "NLKNguyen/papercolor-theme", lazy = false, priority = 100 },
   { "reedes/vim-colors-pencil", lazy = false, priority = 100 },
+  { "miikanissi/modus-themes.nvim", lazy = false, priority = 100 },
 }
